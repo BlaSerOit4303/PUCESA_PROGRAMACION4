fc1385caa4dff72199c687f9a8ed6c430a3916b9
BlaSerOit4303 <caortega.calle2014@gmail.com>
2025-04-11 12:20:59 -0500

    Programacion4

---
+++ for.py
@@ -0,0 +1,36 @@
+print("Programa realizado por Carlos Ortega")
+
+#a=int(input("INgrese el numero: "))
+#for i in range(a):
+#    print("El numero es: ",i)
+
+#for i in range[1,2,5,2212,442,2]:
+    #print("edison", end="") #El end imprime horizontalmente 
+    
+#Tablas y doble for o matriz
+#for i in range(0,6):
+#    print("La tabla es: ",i)
+#    for j in range(0,6):
+#        print(i,"x",j, "=",i*j)
+#While desde -10 hasta +10 e identifique cual es par y cual impar
+
+#a= 10
+#while a>=-10 :
+#    if (a %2 == 0):
+#        print( a, "Es par")
+#    else:
+#        print(a, " Es impar:")
+#    a-=1
+
+#Defs y funciones con objetos
+#Los argumentos se pueden pasar de 2 formas:
+#Argumentos Posicionales: Se asocian a los parámetros de la función en el mismo orden en el que aparecen en la definición de la función 
+#Argumentos nominales: Se indica explícitamente el nombre dle parámetro al que se asocia un argumento de la forma parámetro= argumento.
+
+# def new_func():
+#     def nombre():
+#         nombre= input("Introduce tu nombre: ")
+#         return nombre
+#     print(nombre())
+
+#
